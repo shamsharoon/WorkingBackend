8352e1a2388419b6009e139cfc64cebba449cb9b
shamsharoon <shamsuddin.haroon@ontariotechu.net>
2024-06-17 17:00:27 -0400

    fixed design

---
+++ pwa/urls.py
@@ -0,0 +1,9 @@
+# pwa/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("analyze_picture/", views.analyze_picture, name="analyze_picture"),
+    path("app/", views.app, name="app"),
+]
