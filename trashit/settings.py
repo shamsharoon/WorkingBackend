06587fc7189591a0b2b2640da7fb0972b2eaeb92
Vincent Wong <vincent.wong3@ontariotechu.net>
2024-06-18 16:19:56 -0400

    collected static

--- trashit/settings.py
+++ trashit/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-hiifz5gxeygd8laad=n!h!&gdm_f0a)jlme*y_u0uc2n6cg+df"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["trash-it.azurewebsites.net", "127.0.0.1", "trashtech.azurewebsites.net"]
 
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static",]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
