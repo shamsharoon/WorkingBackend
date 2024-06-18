5f51e7eabb7bc515bf534d91851aec79d7c76c04
shamsharoon <shamsuddin.haroon@ontariotechu.net>
2024-06-17 23:07:52 -0400

    fixed urls.py

--- pwa/urls.py
+++ pwa/urls.py
@@ -1,9 +1,11 @@
 # pwa/urls.py
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("analyze_picture/", views.analyze_picture, name="analyze_picture"),
     path("app/", views.app, name="app"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
