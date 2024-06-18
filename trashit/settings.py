05a7987a72dae93a7dc2df3094747a24aa0f2a3d
shamsharoon <shamsuddin.haroon@ontariotechu.net>
2024-06-17 22:54:51 -0400

    fixed static

--- trashit/settings.py
+++ trashit/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = "trashit.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Corrected path
     }
 }
 
